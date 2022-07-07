3652726fd07c82773149ce3eb7ef80785efbd863
OgechiEzeukwu <ogechiezeukwu17@gmail.com>
2022-07-07 07:45:07 -0700

    My commits

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests functions that print if integer is positive or negative
+ * Return 0
+ */
+int main (void)
+{
+int i;
+
+i = 0
+positive_or_negative(i)
+
+Return 0
+
+
+
+
